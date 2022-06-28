d02f5af7f5c2b58c5349a291f99a4023610b6755
André Ramos <andre_ramosdovale@outlook.com>
2022-06-28 13:04:31 -0300

    Feature: Fix Order Entity with jsonignore

---
+++ src/main/java/com/example/CRUD/spring/service/OrderService.java
@@ -0,0 +1,24 @@
+package com.example.CRUD.spring.service;
+
+import com.example.CRUD.spring.entities.Order;
+import com.example.CRUD.spring.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> order = repository.findById(id);
+        return order.get();
+    }
+}
