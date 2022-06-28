ce15b2f92541416e288b30e444e7655a77eb6706
André Ramos <andre_ramosdovale@outlook.com>
2022-06-28 18:25:10 -0300

    Feature: Implement Category Entity/Repository/Resource/Service

---
+++ src/main/java/com/example/project/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.project.repositories;
+
+import com.example.project.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {}
